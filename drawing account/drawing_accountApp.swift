90e5941165e22e8d7c7a37f4245108fc1515ec92
zih syuan <ziisyuan@gmail.com>
2025-09-19 15:58:53 +0800

    Initial Commit

---
+++ drawing account/drawing_accountApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawing_accountApp.swift
+//  drawing account
+//
+//  Created by Zih Syuan Kuo on 2025/9/19.
+//
+
+import SwiftUI
+
+@main
+struct drawing_accountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
